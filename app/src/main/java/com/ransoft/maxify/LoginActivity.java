2c4bed6b48a5f65fc7c7b42ee493ef212d337a2f
Chris <Chris@test.ransoft.co.za>
2018-09-20 17:55:05 +0200

    added google maps activit

--- app/src/main/java/com/ransoft/maxify/LoginActivity.java
+++ app/src/main/java/com/ransoft/maxify/LoginActivity.java
@@ -3,6 +3,7 @@ package com.ransoft.maxify;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -43,6 +44,14 @@ public class LoginActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btn = (Button)findViewById(R.id.email_sign_in_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MapsActivity.class));
+            }
+        });
+
     }
 }
 
