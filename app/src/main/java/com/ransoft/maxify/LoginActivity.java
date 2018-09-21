767b9684c41e17ba67b3a38eba242d76fad92a42
Chris <Chris@test.ransoft.co.za>
2018-09-22 01:22:24 +0200

    maps works but location hardcoded

--- app/src/main/java/com/ransoft/maxify/LoginActivity.java
+++ app/src/main/java/com/ransoft/maxify/LoginActivity.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +20,7 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.text.Editable;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -46,55 +48,101 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity{
+//
+//    ArrayList<User> user_profile=new ArrayList<>();
+//
+//    private Connection connect=null;
+//    private Statement statement = null;
+//    private ResultSet resultSet=null;
+//    String userid;
+    EditText mobile_number;
+    Connection con;
+    String un,pass,db,ip;
 
-    ArrayList<User> user_profile=new ArrayList<>();
-
-    private Connection connect=null;
-    private Statement statement = null;
-    private ResultSet resultSet=null;
-    String userid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+
         Button btn = (Button)findViewById(R.id.email_sign_in_button);
+        mobile_number= (EditText)findViewById(R.id.email);
+        final String me= mobile_number.getText().toString();
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 try {
-//                    readDataBase();
+                  readDataBase(me);
                     startActivity(new Intent(LoginActivity.this, MapsActivity.class));
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
-
-
-
-    }
-
-    public void readDataBase(int phone_number) throws Exception{
-        Class.forName("com.mysql.jdbc.Driver");
-        connect=DriverManager.getConnection("jdbc:mysql://164.160.91.27:3306/ransoftc_maxify?autoReconnect=true&useSSL=false&"+"user=ransoftc_ransoft&password=Builditfromscratch@2018");
-        statement= connect.createStatement();
-        resultSet=statement.executeQuery("SELECT id FROM ransoftc_maxify.pasenger WHERE email='"+phone_number +"'");
-
-        getid(resultSet);
-
-    }
-    public String getid(ResultSet resultSet)throws SQLException {
-
-        while (resultSet.next()) {
-            User user = new User();
-            userid = resultSet.getString("id");
         }
-        System.out.println(userid);
-        return userid;
 
+    public String readDataBase(String phone_number) throws Exception{
+
+ip="164.160.91.27";
+un="ransoftc_ransoft";
+db="ransoftc_maxify";
+pass="Builditfromscratch@2018";
+con = connectionClass(un,pass,db,ip);
+String results="haven't got connections";
+try{
+    con = connectionClass(un,pass,db,ip);
+    if(con==null){
+        results="Check your internet access";
+        System.out.println(results);
+    }else{
+
+        String query="SELECT * FROM pasenger";
+        Statement stmt=con.createStatement();
+        ResultSet rs=stmt.executeQuery(query);
+
+        while(rs.next()){
+            System.out.println(rs.getString(3));
+        }
     }
+}catch(Exception ex){
+    System.out.println(ex);
+}
+return results;
+    }
+
+
+    public Connection connectionClass(String username,String Password, String database, String server){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection=null;
+        String ConnectionURL=null;
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            //ConnectionURL="jdbc:jtds:sqlserver://"+server+database+";user="+username+";password="+Password;
+            ConnectionURL = "jdbc:jtds:sqlserver://" + server + ";"
+                    + "databaseName=" + database + ";user=" + username
+                    + ";password=" + Password + ";" + "ssl=false";
+            connection=DriverManager.getConnection(ConnectionURL);
+
+        }catch (Exception ex){
+
+            System.out.println("error message is :"+ex);
+        }
+
+        return connection;
+    }
+//    public String getid(ResultSet resultSet)throws SQLException {
+//
+//        while (resultSet.next()) {
+//            User user = new User();
+//            userid = resultSet.getString("id");
+//        }
+//        System.out.println(userid);
+//        return userid;
+//
+//    }
 
 }
 
